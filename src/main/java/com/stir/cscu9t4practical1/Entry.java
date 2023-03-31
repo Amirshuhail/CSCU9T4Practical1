03b6b3e4323e59ba89b2786d87380b38d7f03d05
Amirshuhail <114237599+Amirshuhail@users.noreply.github.com>
2023-04-01 03:17:39 +0400

    Update Entry.java

--- src/main/java/com/stir/cscu9t4practical1/Entry.java
+++ src/main/java/com/stir/cscu9t4practical1/Entry.java
@@ -8,6 +8,7 @@ public class Entry {
   private float distance;
   
   public Entry (String n, int d, int m, int y, int h, int min, int s, float dist) {
+    
     name = n;
     Calendar inst = Calendar.getInstance();
     inst.set(y,m-1,d,h,min,s);
